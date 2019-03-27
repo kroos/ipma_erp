65578288fa6160a869e1f56f16e1a3caadab4481
Dhiauddin <dhiauddin@gmail.com>
2019-03-27 12:09:45 +0800

    - done store the data for quotation

--- app/Http/Controllers/Sales/Costing/QuotationController.php
+++ app/Http/Controllers/Sales/Costing/QuotationController.php
@@ -8,6 +8,9 @@ use \App\Model\QuotQuotation;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// load session
+use Session;
+
 // load image library
 use Intervention\Image\ImageManagerStatic as Image;
 
@@ -36,110 +39,87 @@ class QuotationController extends Controller
 		if ($request->has('qs')) {
 			foreach ($request->qs as $k1 => $v1) {
 
-			//	$qt1 = $qt->hasmanyquotsection()->create([
-			//		'section' => $v1['section'],
-			//	]);
+				$qt1 = $qt->hasmanyquotsection()->create([
+					'section' => $v1['section'],
+				]);
 
 				// dd($v1['qssection']);
 
 				foreach($v1['qssection'] as $k2 => $v2){
-				//	$qt2 = $qt1->hasmanyquotsectionitem()->create([
-				//		'item_id' => $v2['item_id'],
-				//		'price_unit' => $v2['price_unit'],
-				//		'description' => $v2['description'],
-				//		'quantity' => $v2['quantity'],
-				//		'uom_id' => $v2['uom_id'],
-				//		'tax_id' => $v2['tax_id'],
-				//		'tax_value' => $v2['tax_value'],
-				//	]);
+					$qt2 = $qt1->hasmanyquotsectionitem()->create([
+						'item_id' => $v2['item_id'],
+						'price_unit' => $v2['price_unit'],
+						'description' => $v2['description'],
+						'quantity' => $v2['quantity'],
+						'uom_id' => $v2['uom_id'],
+						'tax_id' => $v2['tax_id'],
+						'tax_value' => $v2['tax_value'],
+					]);
 
 					foreach ($v2['qsitem'] as $k3 => $v3) {
 
-						// $request->qs[1]['qssection'][2]['qsitem'][2]['image']->store('public/images/quot');
-						// $request->qs[$k1]['qssection'][$k2]['qsitem'][$k3]['image']->store('public/images/quot');
-						// dd($request->file($v3['image']));
-						// var_dump( $request->qs[$k1]['qssection'][$k2]['qsitem'][$k3] );
-						// $request->qs[$k1]['qssection'][$k2]['qsitem'][$k3]['image']->store('public/images/quot');
-						// dd($request->hasFile());
-						// dd($request->file());
-						// print_r($v3);
-						// echo '<br />';
-						// var_dump( file($v3[$k3]['image']) );
-						// var_dump( $request->file("qs[".$k1."][qssection][".$k2."][qsitem][".$k3."]") );
-						// if( !is_null($v3['image']) ) {
-							// $request->qs[$k1]['qssection'][$k2]['qsitem'][$k3]->store('public/images/quot');
-							// var_dump($request->qs[$k1]['qssection'][$k2]['qsitem'][$k3]->store('public/images/quot') );
-							echo '<br />';
-						// }
-						echo '<br />';
-						echo '<br />';
+						// $request->qs[1]['qssection'][2]['qsitem'][2]['image']->store('public/images/quot');					// yang ni jadi
+						// var_dump(array_has( $request->qs[$k1]['qssection'][$k2]['qsitem'][$k3], 'image' ));
 
-						var_dump($v3);
+						if( array_has(  $v3, 'image' ) ) {
 
-						// if( !is_null($v3['image']) ) {
-						// var_dump($v3['image']).'after if<br/>';
-						// }
+							$filename = $v3['image']->store('public/images/quot');
 
-				//		if($request->file()) {
-				//			$filename = $v3->file('image')->store('public/images/quot');
+							$ass1 = explode('/', $filename);
+							$ass2 = array_except($ass1, ['0']);
+							$image = implode('/', $ass2);
 
-				//			$ass1 = explode('/', $filename);
-				//			$ass2 = array_except($ass1, ['0']);
-				//			$image = implode('/', $ass2);
+							// dd($image);
 
-				//			// dd($image);
+							$imag = Image::make(storage_path('app/'.$filename));
 
-				//			$imag = Image::make(storage_path('app/'.$filename));
+							// resize the image to a height of 400 and constrain aspect ratio (auto width)
+							$imag->resize(NULL, 400, function ($constraint) {
+								$constraint->aspectRatio();
+							});
 
-				//			// resize the image to a height of 400 and constrain aspect ratio (auto width)
-				//			$imag->resize(NULL, 400, function ($constraint) {
-				//				$constraint->aspectRatio();
-				//			});
+							$imag->save();
+						} else {
+							$image = NULL;
+						}
 
-				//			$imag->save();
-				//			// dd( array_add($request->except(['image']), 'image', $filename) );
-
-				//			// $u->update(['image' => $image]);
-				//		}
-
-					//	$qt3 = $qt2->hasmanyquotsectionitemattrib()->create([
-					//		'attribute_id' => $v3['attribute_id'],
-					//		'description_attribute' => $v3['description_attribute'],
-					//		'remarks' => $v3['remarks'],
-					//		// 'image' => $image,
-					//	]);
-
+						$qt3 = $qt2->hasmanyquotsectionitemattrib()->create([
+							'attribute_id' => $v3['attribute_id'],
+							'description_attribute' => $v3['description_attribute'],
+							'remarks' => $v3['remarks'],
+							'image' => $image,
+						]);
 					}
 				}
 			}
 		}
 
-	//	if($request->has('qstop')) {
-	//		foreach ($request->qstop as $k4 => $v4) {
-	//			$qt->hasmanytermofpayment()->create([
-	//				'term_of_payment' => $v4['term_of_payment'],
-	//			]);
-	//		}
-	//	}
+		if($request->has('qstop')) {
+			foreach ($request->qstop as $k4 => $v4) {
+				$qt->hasmanytermofpayment()->create([
+					'term_of_payment' => $v4['term_of_payment'],
+				]);
+			}
+		}
 
-	//	if($request->has('qsexclusions')) {
-	//		foreach ($request->qsexclusions as $k5 => $v5) {
-	//			$qt->hasmanyexclusions()->create([
-	//				'exclusion_id' => $v5['exclusion_id'],
-	//			]);
-	//		}
-	//	}
+		if($request->has('qsexclusions')) {
+			foreach ($request->qsexclusions as $k5 => $v5) {
+				$qt->hasmanyexclusions()->create([
+					'exclusion_id' => $v5['exclusion_id'],
+				]);
+			}
+		}
 
-	//	if($request->has('qsremark')) {
-	//		foreach ($request->qsremark as $k6 => $v6) {
-	//			$qt->hasmanyremarks()->create([
-	//				'remark_id' => $v6['remark_id'],
-	//			]);
-	//		}
-	//	}
+		if($request->has('qsremark')) {
+			foreach ($request->qsremark as $k6 => $v6) {
+				$qt->hasmanyremarks()->create([
+					'remark_id' => $v6['remark_id'],
+				]);
+			}
+		}
 
-		// Session::flash('flash_message', 'Data successfully stored!');
-		// return redirect(route('quot.index'));
+		Session::flash('flash_message', 'Data successfully stored!');
+		return redirect(route('quot.index'));
 	}
 
 	public function show(QuotQuotation $quot)
