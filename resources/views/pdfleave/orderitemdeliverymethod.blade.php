6726f542b3381df3780cd28f136cdfb59fbc9e11
Dhiauddin <dhiauddin@gmail.com>
2019-04-17 16:17:29 +0800

    implement multicell on deliverymethodstore for cs_order

--- resources/views/pdfleave/orderitemdeliverymethod.blade.php
+++ resources/views/pdfleave/orderitemdeliverymethod.blade.php
@@ -83,12 +83,123 @@ class PDF extends Fpdf
 	}
 }
 
+class PDF_MC_Table extends PDF {
+	// variable to store widths and aligns of cells, and line height
+		var $widths;
+		var $aligns;
+		var $lineHeight;
+	//Set the array of column widths
+		function SetWidths($w){
+			$this->widths=$w;
+		}
+	//Set the array of column alignments
+		function SetAligns($a){
+			$this->aligns=$a;
+		}
+	//Set line height
+		function SetLineHeight($h){
+			$this->lineHeight=$h;
+		}
+	//Calculate the height of the row
+		function Row($data)
+		{
+	// number of line
+			$nb=0;
+	// loop each data to find out greatest line number in a row.
+			for($i=0;$i<count($data);$i++){
+	// NbLines will calculate how many lines needed to display text wrapped in specified width.
+	// then max function will compare the result with current $nb. Returning the greatest one. And reassign the $nb.
+				$nb=max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+			}
+	
+	//multiply number of line with line height. This will be the height of current row
+			$h=$this->lineHeight * $nb;
+	//Issue a page break first if needed
+			$this->CheckPageBreak($h);
+	//Draw the cells of current row
+			for($i=0;$i<count($data);$i++)
+			{
+	// width of the current col
+				$w=$this->widths[$i];
+	// alignment of the current col. if unset, make it left.
+				$a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+	//Save the current position
+				$x=$this->GetX();
+				$y=$this->GetY();
+	//Draw the border
+				$this->Rect($x,$y,$w,$h);
+	//Print the text
+				$this->MultiCell($w,5,$data[$i],0,$a);
+	//Put the position to the right of the cell
+				$this->SetXY($x+$w,$y);
+			}
+	//Go to the next line
+			$this->Ln($h);
+		}
+		function CheckPageBreak($h)
+		{
+	//If the height h would cause an overflow, add a new page immediately
+			if($this->GetY()+$h>$this->PageBreakTrigger)
+				$this->AddPage($this->CurOrientation);
+		}
+		function NbLines($w,$txt)
+		{
+	//calculate the number of lines a MultiCell of width w will take
+			$cw=&$this->CurrentFont['cw'];
+			if($w==0)
+				$w=$this->w-$this->rMargin-$this->x;
+			$wmax=($w-2*$this->cMargin)*1000/$this->FontSize;
+			$s=str_replace("\r",'',$txt);
+			$nb=strlen($s);
+			if($nb>0 and $s[$nb-1]=="\n")
+				$nb--;
+			$sep=-1;
+			$i=0;
+			$j=0;
+			$l=0;
+			$nl=1;
+			while($i<$nb)
+			{
+				$c=$s[$i];
+				if($c=="\n")
+				{
+					$i++;
+					$sep=-1;
+					$j=$i;
+					$l=0;
+					$nl++;
+					continue;
+				}
+				if($c==' ')
+					$sep=$i;
+				$l+=$cw[$c];
+				if($l>$wmax)
+				{
+					if($sep==-1)
+					{
+						if($i==$j)
+							$i++;
+					}
+					else
+						$i=$sep+1;
+					$sep=-1;
+					$j=$i;
+					$l=0;
+					$nl++;
+				}
+				else
+					$i++;
+			}
+			return $nl;
+		}
+}
+
 // cari csOrder
 $arr = $request->orderitem;
 $induk = CSOrderItem::find($arr[0])->belongtoorder;
 
 // Instanciation of inherited class
-	$pdf = new Pdf('P','mm', 'A4');
+	$pdf = new PDF_MC_Table('P','mm', 'A4');
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
 	$pdf->SetTitle('Customer Order Items/Parts');
@@ -158,15 +269,35 @@ $induk = CSOrderItem::find($arr[0])->belongtoorder;
 $item = CSOrderItem::whereIn('id', $arr)->get();
 
 	$pdf->SetFont('Arial', NULL, 9);
+
+	// starting PDF_MC_Table
+	// set width for each column (5 columns)
+	$pdf->SetWidths([10, 90, 20, 20, 50]);
+
+	// set alignment
+	$pdf->SetAligns(['C', 'L', 'C', 'C', 'L']);
+
+	// set line heights. This is the height of each lines, not rows.
+	$pdf->SetLineHeight(5);
+
 	foreach($item as $it) {
-		$pdf->Cell(10, 10, $it->id, 'LB', 0, 'C');
-		$pdf->Cell(90, 5, $it->order_item, 'L', 2, 'L');
-		$pdf->Cell(90, 5, $it->item_additional_info, 'LB', 0, 'L');
-		$pdf->SetY( $pdf->GetY() - 5 );
-		$pdf->SetX( 110 );
-		$pdf->Cell(20, 10, $it->quantity, 'LB', 0, 'C');
-		$pdf->Cell(20, 10, $it->belongtoorderstatus->order_item_status, 1, 0, 'C');
-		$pdf->Cell(0, 10, $it->description, 'LBR', 1, 'C');
+
+		$pdf->Row([
+			$it->id,
+			$it->order_item."\r\n".$it->item_additional_info,
+			$it->quantity,
+			$it->belongtoorderstatus->order_item_status,
+			$it->description
+		]);
+
+		// $pdf->Cell(10, 10, $it->id, 'LB', 0, 'C');
+		// $pdf->Cell(90, 5, $it->order_item, 'L', 2, 'L');
+		// $pdf->Cell(90, 5, $it->item_additional_info, 'LB', 0, 'L');
+		// $pdf->SetY( $pdf->GetY() - 5 );
+		// $pdf->SetX( 110 );
+		// $pdf->Cell(20, 10, $it->quantity, 'LB', 0, 'C');
+		// $pdf->Cell(20, 10, $it->belongtoorderstatus->order_item_status, 1, 0, 'C');
+		// $pdf->Cell(0, 10, $it->description, 'LBR', 1, 'C');
 	}
 
 	$pdf->Cell(40, 10, 'Special Request', 'LBR', 0, 'L');
@@ -188,7 +319,7 @@ $item = CSOrderItem::whereIn('id', $arr)->get();
 	$pdf->SetFont('Arial', NULL, 9);
 	$pdf->Cell(40, 5, 'Delivery Remarks:', 0, 0, 'L');
 	$pdf->SetFont('Arial', 'B', 9);
-	$pdf->Cell(0, 5, CSOrderItem::find($arr[0])->delivery_remarks, 0, 1, 'L');
+	$pdf->MultiCell(0, 5, CSOrderItem::find($arr[0])->delivery_remarks, 0, 'L');
 
 
 	// 43mm from bottom
